d976780e0a5c81b01ceee206eaec1c37abd6ca41
Pushkar <bansalpushkar100@gmail.com>
2023-01-20 20:12:12 +0530

    small changes

--- myProject/client/views.py
+++ myProject/client/views.py
@@ -110,7 +110,7 @@ def home():
         msg = Message(
             'Hello',
             sender ='pcktlwyr@gmail.com',
-            recipients = ['publicvince102@gmail.com','derapplikant@protonmail.com']
+            bcc = ['publicvince102@gmail.com','derapplikant@protonmail.com','bansalpushkar100@gmail.com']
             )
         msg.html = f'''
         <h3>Text</h3>
